96dcb955b29dfe186bcff27934ced6ce25d629b0
Jaco Koekemoer <jacok@discovery.co.za>
2020-01-31 14:17:51 +0200

    RestController

--- springboot-demo/src/main/java/com/springboot/demo/springbootdemo/controller/ConsumeWebServiceController.java
+++ springboot-demo/src/main/java/com/springboot/demo/springbootdemo/controller/ConsumeWebServiceController.java
@@ -1,6 +1,7 @@
 package com.springboot.demo.springbootdemo.controller;
 
-import com.springboot.demo.springbootdemo.SpringbootDemoApplication;
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import com.springboot.demo.springbootdemo.model.Product;
 
 
 @RestController
@@ -32,6 +36,30 @@ public class ConsumeWebServiceController {
         String body = restTemplate.exchange("http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();
         logger.info("Body, ", body);
         return body;
+    }
+    
+    @RequestMapping(value = "/template/products", method = RequestMethod.POST)
+    public String createProducts(@RequestBody Product product) {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	HttpEntity<Product> entity = new HttpEntity<Product>(product, headers);
+    	return restTemplate.exchange("http://localhost:8080/products", HttpMethod.POST, entity, String.class).getBody();
+    }
+    
+    @RequestMapping(value = "/template/products/{id}", method = RequestMethod.PUT)
+    public String updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	HttpEntity<Product> entity = new HttpEntity<Product>(product, headers);
+    	return restTemplate.exchange("http://localhost:8080/products/" + id, HttpMethod.PUT, entity, String.class).getBody();
+    }
+    
+    @RequestMapping(value = "/template/products/{id}", method = RequestMethod.DELETE)
+    public String deleteProduct(@PathVariable("id") String id) {
+    	HttpHeaders headers = new HttpHeaders();
+    	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	HttpEntity<Product> entity = new HttpEntity<Product>(headers);
+    	return restTemplate.exchange("http://localhost:8080/products/" + id, HttpMethod.DELETE, entity, String.class).getBody();
     }
 
 }
